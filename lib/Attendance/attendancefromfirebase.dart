3c884e9222443ad5fa149b56be768fe81031eca4
sujit471 <108291389+sujit471@users.noreply.github.com>
2024-03-06 18:12:39 +0545

    Update attendancefromfirebase.dart

--- lib/Attendance/attendancefromfirebase.dart
+++ lib/Attendance/attendancefromfirebase.dart
@@ -1,3 +1,4 @@
+//get the attendance count of the student from firebase using firebase auth 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
